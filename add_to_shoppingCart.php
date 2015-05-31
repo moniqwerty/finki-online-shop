eca37aaed392501b4e66561ac5168e8022e675df
TamaraChemerikikj <tamara.chemerikikj@gmail.com>
2015-05-31 02:37:59 -0700

    Quantity increases when adding same product

--- add_to_shoppingCart.php
+++ add_to_shoppingCart.php
@@ -13,7 +13,24 @@ else {
 	//$cart=mysqli_query($conn, "SELECT * FROM cart WHERE user_id LIKE '$user_id'"); 
 	//if(!$cart)
 		//nova koshnichka
-		$sql="INSERT INTO cart (cart_id, user_id, product_id, quantity) VALUES (NULL, ".$user_id.",".$product_id.",".$quantity.")"; 
+		$isInCart = "SELECT count(*) FROM `cart` WHERE user_id=".$user_id." and product_id=".$product_id;
+		if ($res=mysqli_query($link, $isInCart)){
+			$row = mysqli_fetch_row($res);
+			$resInt = $row[0];
+				
+			$sql="";
+			if (intval($resInt)==0){
+				$sql="INSERT INTO cart (cart_id, user_id, product_id, quantity) VALUES (NULL, ".$user_id.",".$product_id.",".$quantity.")";
+			}
+			else {
+				$quantity = $quantity + 1;
+				$sql="UPDATE `cart` set quantity = ".$quantity." where user_id=".$user_id." and product_id=".$product_id;
+			}
+			echo $sql;
+		}
+		else {
+			echo mysqli_error($link);
+		}
 	/* else 
 	{	
 		//koshnichkata na korisnikot
